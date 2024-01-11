5eacf2182eb0a8671f984e9a255d36fce4c0673a
Tahir <i.tahirnazir@gmail.com>
2024-01-11 09:21:43 +0500

    🎉 init with nuxt 3(ts), vuetify 3

---
+++ layouts/default.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+
+const title = ref("Modernize - Nuxt3 Typescript based Free Admin Dashboard Template");
+useHead({
+  meta: [{ content: title }],
+  titleTemplate: (titleChunk) => {
+    return titleChunk
+      ? `${titleChunk} - Nuxt3 Typescript based Free Admin Dashboard Template`
+      : "Modernize - Nuxt3 Typescript based Free Admin Dashboard Template";
+  },
+});
+</script>
+
+<template>
+    <v-locale-provider >
+        <v-app>
+            <LayoutFullMain/>
+            <v-main>
+                <v-container fluid class="page-wrapper">
+                    <div class="maxWidth">
+                        <NuxtPage  />
+                    </div>
+                </v-container>
+            </v-main>
+        </v-app>
+    </v-locale-provider>
+</template>
